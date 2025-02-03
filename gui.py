56d44d7044385533c9070aca2e87fd3f134ddb8e
DESKTOP-8SENJAR\User <saillor209@gmail.com>
2025-02-03 16:08:57 +0900

    no message

--- gui.py
+++ gui.py
@@ -9,28 +9,20 @@ import preferences
 # 프로그램 종료시 실행되는 함수
 def close_program():
     preferences.save_config()
-    print("QUit")
     root.destroy()
 
 # 로그 출력 함수
-def add_log(message="hi"):
+def print_log(message):
     timestamp = datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
-    log_message = f"{timestamp} {message}"
+    log_message = f"\n{timestamp}\n{message}"
     
     log_text.config(state="normal")  # 편집 가능하게 설정
-    log_text.insert(END, "\n" + log_message)  # 로그 추가
+    log_text.insert(END, log_message)  # 로그 추가
     log_text.config(state="disabled")  # 다시 읽기 전용으로 설정
     if preferences.log_auto_scroll: log_text.yview(END)  # yviw(END) = 스크롤을 가장 아래로 이동
 
-# Front Panel - Clear
-def clear_log():
-    log_text.config(state="normal")
-    log_text.delete("1.0", END)  # 첫 번째 문자(1.0)부터 끝(END)까지 삭제
-    log_text.insert(END, f"{datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")}\n[Program] The log has been cleared.")
-    log_text.config(state="disabled")
-
 # 새 윈도우를 중앙에 오도록 위치 계산해주는 함수
-def center_calc(window_width, window_height):
+def center_calc(window_width, window_height, offset):
 
     # root 창의 위치와 크기 가져오기
     root_x = root.winfo_x()  # root 창의 X 좌표
@@ -40,18 +32,14 @@ def center_calc(window_width, window_height):
     
     # 중앙 좌표 계산
     x_position = root_x + (root_width - window_width) // 2
-    y_position = root_y + (root_height - window_height) // 2 + 20  # 20 = 보정값
+    y_position = root_y + (root_height - window_height) // 2 + offset  # offset = 보정값(낮을수록 창이 올라감)
     return window_width, window_height, x_position, y_position
 
 # 해상도
 window_width = 640
 window_height = 480
 
-ico = ("./assets/hk4e_global.ico")
-
 root = Tk()  # 메인 GUI 창 생성
-
-root.resizable(False, False)  # 창 크기 조절 불가능
 
 # 메인 디스플레이 해상도 가져오기
 screen_width = root.winfo_screenwidth()
@@ -62,7 +50,11 @@ x_position = (screen_width - window_width) // 2
 y_position = (screen_height - window_height) // 2
 
 root.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")  # 창의 위치 설정
+root.resizable(False, False)  # 창 크기 조절 불가능
+
+ico = ("./assets/hk4e_global.ico")
 root.iconbitmap(ico)  # 아이콘 지정
+
 root.title("Upscaler")  # 창 제목 설정
 
 ## 로그 출력 창 (ScrolledText)
@@ -70,17 +62,24 @@ root.title("Upscaler")  # 창 제목 설정
 log_text = ScrolledText(root, bg="black", fg="white", insertbackground="white", width=10, height=10, state="normal")
 # padx=가로여백, pady=세로여백, fill=빈공간 채우는 방식, expand=창에 따른 위젯 크기 조정
 log_text.pack(padx=12, pady=12, fill=BOTH, expand=True)
-log_text.insert(END, f"{datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")}\n[Program] Hi")
+log_text.insert(END, f"{datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")}\nHi")
 log_text.config(state="disabled")
 if preferences.log_auto_scroll: log_text.yview(END)
 
    ### Front Panel ###
 
+# Front Panel - Clear
+def clear_log():
+    log_text.config(state="normal")
+    log_text.delete("1.0", END)  # 첫 번째 문자(1.0)부터 끝(END)까지 삭제
+    log_text.insert(END, f"{datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")}\nThe log has been cleared.")
+    log_text.config(state="disabled")
+
 # 로그 테스트 버튼
-log_button = Button(root, text="log_test", command=add_log)
+log_button = Button(root, text="log_test", command=lambda: print_log("Hi"))
 log_button.pack(side=LEFT, padx=(12, 0), pady=(0, 12))
 # 스탑 버튼(아직 기능이 없음 ㅋㅋ)
-stop_button = Button(root, text="Stop", command=add_log)
+stop_button = Button(root, text="Stop", command=print_log)
 stop_button.pack(side=LEFT, padx=(12, 0), pady=(0, 12))
 # Clear Log
 clear_button = Button(root, text="Claer", command=clear_log)
@@ -97,14 +96,15 @@ root.config(menu=menu_bar)  # 메뉴 바를 창에 적용
 
 # File - Prefernces method
 def file_preference():
-    width, height, x, y = center_calc(500, 400)
+    width, height, x, y = center_calc(500, 400, 25)
     prefer = Toplevel(root)
-    prefer.resizable(False, False)
     prefer.geometry(f"{width}x{height}+{x}+{y}")
-    prefer.iconbitmap(ico)
-    prefer.title("Preferences")
+    prefer.resizable(False, False)
     prefer.transient(root)
     prefer.grab_set()
+    prefer.iconbitmap(ico)
+    prefer.title("Preferences")
+    prefer.mainloop()
 
 # FIle 메뉴 생성
 file_menu = Menu(menu_bar, tearoff=0)
@@ -117,13 +117,15 @@ file_menu.add_command(label="Quit", command=close_program)
 
 # Select input path...
 def edit_select_input_path():
-    path = askdirectory(title="select the path of the video...")
+    path = askdirectory(title="Select the path of the video...")
     preferences.video_input_path = path
+    print_log(f"The input path \"{path}\" has been selected.")
 
 # Select output path...
 def edit_select_output_path():
-    path = askdirectory(title="select the path of the video...")
+    path = askdirectory(title="Select the path of the video...")
     preferences.video_output_path = path
+    print_log(f"The output path \"{path}\" has been selected.")
 
 def input_print():
     print(preferences.video_input_path)
@@ -141,15 +143,20 @@ edit_menu.add_command(label="test", command=input_print)
 
 # Help - Information... 실행
 def help_information():
-    width, height, x, y = center_calc(400, 300)
+    width, height, x, y = center_calc(400, 130, 15)
     about = Toplevel(root)
-    about.resizable(False, False)
     about.geometry(f"{width}x{height}+{x}+{y}")
-    about.iconbitmap(ico)
-    about.title("Information...")
+    about.resizable(False, False)
     about.transient(root)  # 메인 창과 연관되도록 설정
     about.grab_set()  # info 창이 닫히기 전까지 다른 창 클릭 방지
-
+    about.iconbitmap(ico)
+    about.title("Information...")
+    # 텍스트 라벨
+    text = Label(about, text="This software is licensed under GNU AGPL version 3.\nCopyright (C) 2025 saillor209.\nSource code is available on GitHub.")
+    text.pack(padx=12, pady=(12, 0))
+    # Close 버튼
+    close = Button(about, text="Close", command=about.destroy)
+    close.pack(side=BOTTOM, pady=(0, 12))
 
     about.mainloop()
 
--- preferences.py
+++ preferences.py
@@ -5,15 +5,30 @@ def print_settings():
     print(f"{log_auto_scroll}, {video_input_path}")
 
 ## 모든 설정을 기본 값으로 설정하는 함수
-def set_default():
-    global log_auto_scroll, video_input_path, video_output_path, ffmpeg_calc_method, ffmpeg_hwaccel, ffmpeg_codec, realcugan_model
-    log_auto_scroll = True
-    video_input_path = "./.Videos"
-    video_output_path = "./.Results"
-    ffmpeg_calc_method = "cpu"
-    ffmpeg_hwaccel = False if ffmpeg_calc_method == "cpu" else True
-    ffmpeg_codec = "264"
-    realcugan_model = "model_pro"
+def set_preferences(preset = "default"):
+    global log_auto_scroll, video_input_path, video_output_path, ffmpeg_calc_method, ffmpeg_hwaccel, ffmpeg_codec, realcugan_model  # 전역변수 설정
+    if preset == "default":
+        ## GUI 설정
+        log_auto_scroll = True
+        ## 경로 설정
+        video_input_path = "./.Videos"
+        video_output_path = "./.Results"
+        ## FFmpeg 설정
+        ffmpeg_calc_method = "cpu"  # cpu (hwaccel = False) / cuda (Nvidia) / qsv (Intel Quick Sync)
+        if ffmpeg_calc_method == "cpu": ffmpeg_hwaccel = False
+        else: ffmpeg_hwaccel = True  # 연산 주체가 cpu라면 하드웨어 가속은 False가 되어야 합니다.
+        ffmpeg_codec = "264"  # 264 (H.264) / 265 (H.265)
+        ### realcugan 설정 ###
+        realcugan_model = "model_pro"  # model_pro / model_se
+    else:
+        with open(f"./configs/{preset}.json", "r", encoding="utf-8") as f:
+            config = json.load(f)  # JSON 내용을 딕셔너리로 변환
+        log_auto_scroll = config["log_auto_scroll"]
+        video_input_path = config["video_input_path"]
+        video_output_path = config["video_output_path"]
+        ffmpeg_calc_method = config["ffmpeg_calc_method"]
+        ffmpeg_codec = config["ffmpeg_codec"]
+        realcugan_model = config["realcugan_model"]
 
 ## 프로그램 종료 시 config.json을 저장함.
 def save_config(filename="./configs/config.json"):
@@ -30,19 +45,11 @@ def save_config(filename="./configs/config.json"):
         json.dump(data, file, indent=4)
 
 ## 최초 preferences 설정
-if os.path.exists("./configs/config.json"):  # config.json 파일 존재 시 config.json을 불러옴
-    with open("./configs/config.json", "r", encoding="utf-8") as f:
-        config = json.load(f)  # JSON 내용을 딕셔너리로 변환
-    ## GUI 설정
-    log_auto_scroll = config["log_auto_scroll"]
-    ## 경로 설정
-    video_input_path = config["video_input_path"]
-    video_output_path = config["video_output_path"]
-    ## FFmpeg 설정
-    ffmpeg_calc_method = config["ffmpeg_calc_method"]# cpu (hwaccel = False) / cuda (Nvidia) / qsv (Intel Quick Sync)
-    if ffmpeg_calc_method == "cpu": ffmpeg_hwaccel = False
-    else: ffmpeg_hwaccel = True  # 연산 주체가 cpu라면 하드웨어 가속은 False가 되어야 합니다.
-    ffmpeg_codec = config["ffmpeg_codec"]  # 264 (H.264) / 265 (H.265)
-    ### realcugan 설정 ###
-    realcugan_model = config["realcugan_model"]  # model_pro / model_se
-else: set_default()
+
+if not os.path.exists("configs"):  # configs 경로 없으면 생성
+    os.makedirs("configs")
+
+if not os.path.exists("./configs/config.json"):  # config.json 파일 미존재 시 기본값
+    set_preferences()
+else:  # config.json 파일 존재 시 불러오기
+    set_preferences("config")
